a1686ff559f0404a1d5e55882ebdf4c78a7753c4
Phoenix Bishea <phoenixbishea@gmail.com>
2016-09-07 16:30:36 -0500

    Add out of bounds respawn box

---
+++ Assets/Scripts/OutOfBounds.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OutOfBounds : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            other.gameObject.GetComponent<LifeManager>().Die();
+        }
+    }
+}
