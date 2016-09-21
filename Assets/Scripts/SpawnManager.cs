49d331cc7eaeb8b26b7165438000b9acff7237e8
Scott Munro <scottnmunro@gmail.com>
2016-09-21 14:50:22 -0500

    preliminary spike script push

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,21 +5,27 @@ namespace Filibusters
     public class SpawnManager : MonoBehaviour
     {
         private GameObject[] SpawnPoints;
+        GameObject LocalPlayer;
 
         // Use this for initialization
         void Start()
         {
             SpawnPoints = GameObject.FindGameObjectsWithTag("Respawn");
             Vector3 spawnPositon = GetRandomSpawnPoint();
-            GameObject localPlayer = PhotonNetwork.Instantiate("NetPlayer", spawnPositon, Quaternion.identity, 0);
-            localPlayer.GetComponent<SimplePhysics>().enabled = true;
+            GameObject LocalPlayer = PhotonNetwork.Instantiate("NetPlayer", spawnPositon, Quaternion.identity, 0);
+            LocalPlayer.GetComponent<SimplePhysics>().enabled = true;
             FollowPlayer followScript = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<FollowPlayer>();
-            followScript.mPlayer = localPlayer;
+            followScript.mPlayer = LocalPlayer;
         }
 
         // Update is called once per frame
         void Update()
         {
+        }
+
+        void RespawnLocalPlayer()
+        {
+            LocalPlayer.transform.position = GetRandomSpawnPoint();
         }
 
         private Vector3 GetRandomSpawnPoint()
