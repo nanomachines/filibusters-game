e29f5aab05a4a1ee065699977bba3fe792cc6e80
Ben Lin <ben.lin9@gmail.com>
2016-09-28 15:01:42 -0500

    added deposit status indicator

---
+++ Assets/Scripts/DepositStatusController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Filibusters
+{
+    public class DepositStatusController : MonoBehaviour {
+
+        SpriteRenderer mSpriteRenderer;
+        Color green = new Color(70 / 255f, 227 / 255f, 110 / 255f, 0.5f);
+        Color red = new Color(227 / 255f, 70 / 255f, 70 / 255f, 0.5f);
+
+        // Use this for initialization
+        void Start() {
+            mSpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        }
+
+        // Update is called once per frame
+        void Update() {
+            DepositManager dm = transform.parent.gameObject.GetComponent<DepositManager>();
+            if (dm.isDepositing())
+            {
+                mSpriteRenderer.color = green;
+            }
+            else if (dm.isBlocked())
+            {
+                mSpriteRenderer.color = red;
+            }
+            else
+            {
+                mSpriteRenderer.color = new Color(0, 0, 0, 0);
+            }
+        }
+    }
+}
