0b4b235f0072f62014a14e60732ccd37f1fa767a
Scott Munro <scottnmunro@gmail.com>
2016-09-28 13:19:26 -0500

    using scene constants

---
+++ Assets/Scripts/Scenes.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Filibusters
+{
+    public class Scenes
+    {
+        public static readonly string SCENE_DIR = "Scenes/";
+        public static readonly string START_MENU = "StartMenu";
+        public static readonly string READY_MENU = "ReadyMenu";
+        public static readonly string MAIN = "Main";
+        public static readonly string GAME_OVER = "GameOver";
+    }
+}
