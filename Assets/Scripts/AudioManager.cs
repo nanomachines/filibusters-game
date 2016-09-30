1246a5c54a05dd8f61f6027fa615ac7c799de975
Scott Munro <scottnmunro@gmail.com>
2016-09-29 19:55:17 -0500

    Built out audio system to play our music and sfx

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Filibusters
+{
+    public class AudioManager : MonoBehaviour
+    {
+        public static AudioManager Instance = null;
+        private AudioSource mSource;
+        [SerializeField]
+        private AudioClip mPlayerDeath;
+
+        // Use this for initialization
+        void Start()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                RegisterEvents();
+                mSource = GetComponent<AudioSource>();
+                DontDestroyOnLoad(this);
+            }
+            else
+            {
+                Destroy(this);
+            }
+        }
+
+        private void RegisterEvents()
+        {
+            EventSystem.OnDeathEvent += (int playerViewId) =>
+            {
+                mSource.PlayOneShot(mPlayerDeath);
+            };
+        }
+    }
+}
