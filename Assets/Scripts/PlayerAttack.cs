72afaf25d2639a36012c0b671e89f8821ad3c909
Phoenix Bishea <phoenixbishea@gmail.com>
2016-10-04 15:29:44 -0500

    Player has notion of ammo and spawning a bullet over the network. The bullet still does not move and is owned by the spawning player.

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Filibusters
+{
+    public class PlayerAttack : MonoBehaviour
+    {
+        private WeaponInventory mWeaponInventory;
+        private PhotonView mPhotonView;
+        private int mViewId;
+
+        void Start()
+        {
+            mWeaponInventory = GetComponent<WeaponInventory>();
+            mPhotonView = GetComponent<PhotonView>();
+            mViewId = gameObject.GetComponent<PhotonView>().viewID;
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                Debug.Log("Fire keycode pressed");
+                if (mWeaponInventory.CanFire())
+                {
+                    Debug.Log("This player CanFire");
+
+                    string bulletName = mWeaponInventory.GetBulletType();
+                    if (bulletName.Length != 0)
+                    {
+                        InstantiateBullet(bulletName);
+                    }
+                    // trigger succesful fire event
+                }
+                else
+                {
+                    // trigger failed fire event
+                }
+            }
+        }
+
+        void InstantiateBullet(string bulletName)
+        {
+            GameObject bullet = PhotonNetwork.Instantiate(bulletName, transform.position, Quaternion.identity, 0);
+        }
+    }
+}
--- Assets/Scripts/WeaponInventory.cs
+++ Assets/Scripts/WeaponInventory.cs
@@ -13,9 +13,17 @@ namespace Filibusters
             set { mPlayerState.mWeaponId = value; }
         }
 
+        private int mAmmo;
+        [SerializeField]
+        private float mCoolDownSeconds;
+        private bool mCoolingDown;
+
         void Start()
         {
             mPlayerState = GetComponent<PlayerState>();
+            // Set ammo to -1 for infinite ammo
+            mAmmo = -1;
+            mCoolingDown = false;
         }
 
         public bool CanEquip()
@@ -28,6 +36,49 @@ namespace Filibusters
             mWeaponId = weaponId;
             int actorId = GetComponent<PhotonView>().owner.ID;
             EventSystem.OnEquipWeapon(actorId, weaponId);
+            // TODO: add ammo counts and cooldown seconds for each weapon as a serialized private field
+            // Set ammo to infinite for now
+            switch (weaponId)
+            {
+                default:
+                    mAmmo = -1;
+                    break;
+            }
+        }
+
+        public bool CanFire()
+        {
+            if (!mCoolingDown && mAmmo != 0)
+            {
+                StartCoroutine(CoolDownTimer());
+                return true;
+            }
+            return false;
+        }
+
+        private IEnumerator CoolDownTimer()
+        {
+            mCoolingDown = true;
+            yield return new WaitForSeconds(mCoolDownSeconds);
+            mCoolingDown = false;
+        }
+
+        public string GetBulletType()
+        {
+            // Decrement ammo count
+            if (mAmmo > 0)
+            {
+                --mAmmo;
+            }
+            // Return string resource name to instantiate
+            if (mWeaponId == WeaponId.VETO)
+            {
+                return "VetoBullet";
+            }
+            else
+            {
+                return "";
+            }
         }
     }
 }
