91116120f0162974d79571587da5c9afd3795ee7
Scott Munro <scottnmunro@gmail.com>
2016-10-11 17:56:58 -0500

    refactor for decoupling and fixing the rest of the ui input systems
    updated so that the "Go!" button's interactivity was not coupled
    to the script that updates the UI's input axises.
    
    updated the rest of the ui event systems to have this dynamic ui
    input axes script and an input wrapper so that each of these screens
    can take advantage of being able to interact with the UI while using
    a controller

--- Assets/Scripts/DynamicUIInputAxis.cs
+++ Assets/Scripts/DynamicUIInputAxis.cs
@@ -3,30 +3,17 @@ using System.Collections;
 
 namespace Filibusters
 {
-	public class UIController : MonoBehaviour
+	public class DynamicUIInputAxis : MonoBehaviour
 	{
-        [SerializeField]
-        private UnityEngine.UI.Button mStartButton;
         private UnityEngine.EventSystems.StandaloneInputModule mInputModule;
 
         public void Start()
         {
             mInputModule = GetComponent<UnityEngine.EventSystems.StandaloneInputModule>();
-            mStartButton.gameObject.SetActive(false);
-            mStartButton.interactable = false;
             mInputModule.horizontalAxis = InputWrapper.LeftXInputName;
             mInputModule.verticalAxis = InputWrapper.LeftYInputName;
-            EventSystem.OnAllPlayersReadyEvent += () =>
-            {
-                mStartButton.gameObject.SetActive(true);
-                mStartButton.interactable = true;
-            };
-            EventSystem.OnAllPlayersNotReadyEvent += () =>
-            {
-                mStartButton.gameObject.SetActive(false);
-                mStartButton.interactable = false;
-            };
         }
+
         public void Update()
         {
             mInputModule.submitButton = InputWrapper.Instance.AnyJoysticksConnected() ?
---
+++ Assets/Scripts/StartGameButtonInteractivity.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Filibusters
+{
+	public class StartGameButtonInteractivity : MonoBehaviour
+	{
+        private UnityEngine.UI.Button mStartButton;
+        private UnityEngine.UI.Image mStartButtonImg;
+		void Start()
+		{
+            mStartButton = GetComponent<UnityEngine.UI.Button>();
+            mStartButton.interactable = false;
+
+            mStartButtonImg = GetComponent<UnityEngine.UI.Image>();
+            mStartButtonImg.enabled = false;
+
+            EventSystem.OnAllPlayersReadyEvent += () =>
+            {
+                mStartButtonImg.enabled = true;
+                mStartButton.interactable = true;
+            };
+            EventSystem.OnAllPlayersNotReadyEvent += () =>
+            {
+                mStartButtonImg.enabled = false;
+                mStartButton.interactable = false;
+            };
+		}
+	}
+}
