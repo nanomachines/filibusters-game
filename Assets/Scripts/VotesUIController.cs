e07468089ad5fd5cb1b5d051b0a17754051cb91b
Scott Munro <scottnmunro@gmail.com>
2016-10-18 20:31:36 -0500

    added a votes ui controller that enables/disables the UI for the vote counts when players start and when they disconnect respectively

---
+++ Assets/Scripts/VotesUIController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using Photon;
+
+namespace Filibusters
+{
+	public class VotesUIController : PunBehaviour
+	{
+        [SerializeField]
+        GameObject[] mVoteUIElements;
+
+		// Use this for initialization
+		void Start()
+		{
+            var activePlayers = NetworkManager.GetActivePlayerNumbers();
+            for (int i = 0; i < GameConstants.MAX_ONLINE_PLAYERS_IN_GAME; ++i)
+            {
+                mVoteUIElements[i].SetActive(activePlayers[i]);
+            }
+		}
+		
+		// Update is called once per frame
+		void Update()
+		{
+		}
+
+        public override void OnPhotonPlayerDisconnected(PhotonPlayer otherPlayer)
+        {
+            int playerNumberToFree = NetworkManager.GetPlayerNumber(otherPlayer);
+            mVoteUIElements[playerNumberToFree].SetActive(false);
+        }
+	}
+}
