c1efd93dc1c69b751d941b9c245899607ac8bca8
Scott Munro <scottnmunro@gmail.com>
2016-10-22 13:46:39 -0500

    can now navigate to how to play menu from main menu

--- Assets/Scripts/StartMenuManager.cs
+++ Assets/Scripts/StartMenuManager.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 namespace Filibusters
@@ -6,6 +8,22 @@ namespace Filibusters
     public class StartMenuManager : Photon.PunBehaviour 
     {
         public static readonly string SESSION_NAME = "Default Session Scott";
+
+        [SerializeField]
+        private Button HostButton;
+
+        [SerializeField]
+        private Button JoinButton;
+
+        [SerializeField]
+        private Button HowToPlayButton;
+
+        void Start()
+        {
+            HostButton.onClick.AddListener(HostGame);
+            JoinButton.onClick.AddListener(JoinGame);
+            HowToPlayButton.onClick.AddListener(DisplayHowToPlay);
+        }
 
         void OnGUI()
         {
@@ -20,6 +38,11 @@ namespace Filibusters
         public void JoinGame()
         {
             NetworkManager.JoinGameSession(SESSION_NAME);
+        }
+
+        public void DisplayHowToPlay()
+        {
+            SceneManager.LoadScene(Scenes.HOW_TO_PLAY);
         }
     
         public override void OnJoinedRoom()
