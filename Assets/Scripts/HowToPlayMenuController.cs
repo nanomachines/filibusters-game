738a66d80ddf8bc56d3b29a16c1fd6987023c8c0
Scott Munro <scottnmunro@gmail.com>
2016-10-22 13:10:26 -0500

    added the slideshow menu with forward and backward navigation that exits to the menu screen

---
+++ Assets/Scripts/HowToPlayMenuController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace Filibusters
+{
+    public class HowToPlayMenuController : MonoBehaviour
+    {
+        [SerializeField]
+        GameObject[] mTutorialSlides;
+        int mCurrentSlide;
+
+        [SerializeField]
+        Button mBackButton;
+
+        [SerializeField]
+        Button mExitButton;
+
+        [SerializeField]
+        Button mNextButton;
+
+        void Start()
+        {
+            mCurrentSlide = 0;
+            mTutorialSlides[mCurrentSlide].SetActive(true);
+            UpdateButtonInteractivity();
+
+            mBackButton.onClick.AddListener(NavigateBack);
+            mNextButton.onClick.AddListener(NavigateNext);
+            mExitButton.onClick.AddListener(ExitHowToPlay);
+        }
+		
+        void NavigateBack()
+        {
+            mTutorialSlides[mCurrentSlide].SetActive(false);
+            mTutorialSlides[--mCurrentSlide].SetActive(true);
+
+            mBackButton.interactable = mCurrentSlide != 0;
+            mNextButton.interactable = mCurrentSlide != (mTutorialSlides.Length - 1);
+            UpdateButtonInteractivity();
+        }
+
+        void NavigateNext()
+        {
+            mTutorialSlides[mCurrentSlide].SetActive(false);
+            mTutorialSlides[++mCurrentSlide].SetActive(true);
+            UpdateButtonInteractivity();
+        }
+
+        void ExitHowToPlay()
+        {
+            PhotonNetwork.LoadLevel(Scenes.START_MENU);
+        }
+
+        void UpdateButtonInteractivity()
+        {
+            mBackButton.interactable = mCurrentSlide != 0;
+            mNextButton.interactable = mCurrentSlide != (mTutorialSlides.Length - 1);
+        }
+    }
+}
