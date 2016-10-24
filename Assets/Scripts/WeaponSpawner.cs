e93d8ce0ca9f0c531378ba60147ae17a0ca330b7
Phoenix Bishea <phoenixb@utexas.edu>
2016-10-23 20:21:53 -0500

    Players can now swap weapons by running over a weapon and pressing e or x with a controller

--- Assets/Scripts/WeaponSpawner.cs
+++ Assets/Scripts/WeaponSpawner.cs
@@ -8,8 +8,13 @@ namespace Filibusters
     {
         private bool mHasBeenCollected;
         private SpriteRenderer mRenderer;
-        private BoxCollider2D mCollider;
         private PhotonView mPhotonView;
+
+        private BoxCollider2D mCollider;
+        private Vector2 mTLCorner;
+        private Vector2 mBRCorner;
+        [SerializeField]
+        private LayerMask mPlayerLayer;
 
         // If SecondsToRespawn is less than zero that means
         // that we don't want weapons to respawn (i.e. in the 
@@ -24,21 +29,38 @@ namespace Filibusters
         {
             mHasBeenCollected = false;
             mRenderer = GetComponent<SpriteRenderer>();
-            mCollider = GetComponent<BoxCollider2D>();
             mPhotonView = GetComponent<PhotonView>();
+
+            mCollider = GetComponent<BoxCollider2D>();
+
+            float halfX = mCollider.bounds.extents.x;
+            float halfY = mCollider.bounds.extents.y;
+            float offsetX = mCollider.offset.x;
+            float offsetY = mCollider.offset.y;
+
+            mTLCorner = transform.TransformPoint(offsetX - halfX, offsetY + halfY, 0f);
+            mBRCorner = transform.TransformPoint(offsetX + halfX, offsetY - halfY, 0f);
         }
 
-        void OnTriggerEnter2D(Collider2D other)
+        void Update()
         {
-            if (other.tag == Tags.PLAYER &&
-                other.gameObject.GetComponent<WeaponInventory>().CanEquip())
+            if (InputWrapper.Instance.DropWeaponPressed)
+            {
+                CheckForPlayerCollision();
+            }
+        }
+
+        void CheckForPlayerCollision()
+        {
+            Collider2D other;
+            if (other = Physics2D.OverlapArea(mTLCorner, mBRCorner, mPlayerLayer))
             {
                 Debug.Log("weapon");
                 // this is an id that maps 1-to-1 with the player who collected the weapon
                 int viewId = other.gameObject.GetComponent<PhotonView>().viewID;
                 mPhotonView.RPC("OnWeaponTriggered", PhotonTargets.MasterClient, viewId);
             }
-        }
+        }     
 
         [PunRPC]
         public void OnWeaponTriggered(int viewId)
