3da11a6ff0dbe1c07c415f03d230a3b3122c59cc
Scott Munro <scottnmunro@gmail.com>
2016-10-28 11:12:00 -0500

    Added errors toasts as well

--- Assets/Scripts/HostSession.cs
+++ Assets/Scripts/HostSession.cs
@@ -1,11 +1,19 @@
-﻿using UnityEngine.UI;
-using Photon;
+﻿using Photon;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 
 namespace Filibusters
 {
     public class HostSession : PunBehaviour
     {
         InputField mSessionNameField;
+
+        [SerializeField]
+        GameObject mErrorText;
+
+        [SerializeField]
+        float mToastTime;
 
         void Start()
         {
@@ -21,6 +29,19 @@ namespace Filibusters
         public override void OnJoinedRoom()
         {
             PhotonNetwork.LoadLevel(Scenes.READY_MENU);
+        }
+
+        public override void OnPhotonCreateRoomFailed(object[] codeAndMsg)
+        {
+            mErrorText.GetComponent<Text>().text = (string)codeAndMsg[1];
+            StartCoroutine(ToastErrorText());
+        }
+
+        private IEnumerator ToastErrorText()
+        {
+            mErrorText.SetActive(true);
+            yield return new WaitForSeconds(mToastTime);
+            mErrorText.SetActive(false);
         }
     }
 }
