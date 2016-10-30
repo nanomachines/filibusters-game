2d511ff19fa0089d8ac9f810d3164e577e0a08ae
Scott Munro <scottnmunro@gmail.com>
2016-10-30 16:59:19 -0500

    Added back to main menu button

---
+++ Assets/Scripts/BackToMainMenuButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+namespace Filibusters
+{
+    public class BackToMainMenuButton : MonoBehaviour
+    {
+        void Start()
+        {
+            GetComponent<UnityEngine.UI.Button>().onClick.AddListener(
+                () => {  SceneManager.LoadScene(Scenes.START_MENU); }
+            );
+        }
+    }
+}
