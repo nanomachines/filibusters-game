d181cce92e25b22df5e87bc219d47918a77cf2bb
Phoenix Bishea <phoenixbishea@gmail.com>
2016-11-02 00:15:07 -0500

    Create an overlay to return to the main menu and a script to control it. Update UI Event System manager in the main scene.

---
+++ Assets/Scripts/LeaveGameManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Filibusters
+{
+	public class LeaveGameManager : MonoBehaviour 
+	{
+        [SerializeField]
+        private GameObject mLeaveOverlay;
+        private bool mOverlayEnabled;
+
+        [SerializeField]
+        private UnityEngine.UI.Button mYesButton;
+        [SerializeField]
+        private UnityEngine.UI.Button mNoButton;
+
+		void Start() 
+		{
+            mOverlayEnabled = false;
+            mYesButton.onClick.AddListener(() => { Utility.BackToStartMenu(); });
+            mNoButton.onClick.AddListener(() => { ToggleOverlay(false); });
+        }
+		
+		void Update() 
+		{
+		    if (InputWrapper.Instance.CancelPressed)
+            {
+                ToggleOverlay(!mOverlayEnabled);
+            }
+		}
+
+        void ToggleOverlay(bool isEnabled)
+        {
+            mOverlayEnabled = isEnabled;
+            mLeaveOverlay.SetActive(isEnabled);
+        }
+	}
+}
