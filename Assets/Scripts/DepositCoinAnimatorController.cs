9d4129eb6143c23a0ea5cc93b5c5136d3ad6ef99
Scott Munro <scottnmunro@gmail.com>
2016-11-06 11:32:26 -0600

    added the coin deposit animation

---
+++ Assets/Scripts/DepositCoinAnimatorController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Filibusters
+{
+    public class DepositCoinAnimatorController : MonoBehaviour
+    {
+        Animator mDepositCoinAnimator;
+        [SerializeField]
+        AnimationCurve mMotionCurve;
+        Vector3 mOrigin;
+        Vector3 mYDisplacement; 
+
+        void Start()
+        {
+            mDepositCoinAnimator = GetComponent<Animator>();
+            EventSystem.OnCoinDepositedEvent += StartAnimation;
+
+            mOrigin = transform.position;
+            mYDisplacement = new Vector3(0, 1f, 0);
+        }
+
+        void OnDestroy()
+        {
+            EventSystem.OnCoinDepositedEvent -= StartAnimation;
+        }
+
+        void StartAnimation(int ownerId, int newDepositBalance)
+        {
+            StartCoroutine(RunAnimation());
+        }
+
+        IEnumerator RunAnimation()
+        {
+            mDepositCoinAnimator.SetBool("DepositCoinAnimation", true);
+
+            float time = 0f;
+            while (time < 1f)
+            {
+                var displacementRatio = mMotionCurve.Evaluate(time);
+                transform.position = mYDisplacement * displacementRatio + mOrigin;
+                time += Time.deltaTime;
+                yield return new WaitForFixedUpdate();
+            }
+
+            mDepositCoinAnimator.SetBool("DepositCoinAnimation", false);
+        }
+    }
+}
