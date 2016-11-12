1cb242f0bf8f3d854a4e8326ea2d3a26443dfbb1
Scott Munro <scottnmunro@gmail.com>
2016-11-12 13:23:23 -0600

    cleanedup and removed methodB artifacts

--- Assets/Scripts/ParallaxController.cs
+++ Assets/Scripts/ParallaxController.cs
@@ -5,8 +5,6 @@ namespace Filibusters
 {
     public class ParallaxController : MonoBehaviour
     {
-        [SerializeField]
-        float zFactor;
         Transform mCameraTransform;
 
         void Start()
@@ -26,16 +24,6 @@ namespace Filibusters
                 var yOffset = camY - (camY / groupZ);
                 zGroupTransform.position = new Vector3(xOffset, yOffset, groupZ);
             }
-            /*
-            foreach (Transform zGroupTransform in transform)
-            {
-                var origin = zGroupTransform.gameObject.GetComponent<Origin>().position;
-                var z = 1 / (zGroupTransform.localScale.y * .5f) * zFactor;
-                var xOffset = (camX - (camX / z)) + origin.x;
-                var yOffset = (camY - (camY / z)) + origin.y;
-                zGroupTransform.position = new Vector3(xOffset, yOffset, zGroupTransform.position.z);
-            }
-            */
         }
     }
 }
