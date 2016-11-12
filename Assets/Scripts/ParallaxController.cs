79eb4c074e8caa4d000be919eac05ec50a128a88
Scott Munro <scottnmunro@gmail.com>
2016-11-12 13:19:56 -0600

    settled on using method A. had to do some tweaks to get it just right moved the builds back a bit and added an extra building to the mid layer

--- Assets/Scripts/ParallaxController.cs
+++ Assets/Scripts/ParallaxController.cs
@@ -19,7 +19,6 @@ namespace Filibusters
             Vector2 cameraPos = mCameraTransform.position;
             var camX = mCameraTransform.position.x;
             var camY = mCameraTransform.position.y;
-            /*
             foreach (Transform zGroupTransform in transform)
             {
                 var groupZ = zGroupTransform.position.z;
@@ -27,7 +26,7 @@ namespace Filibusters
                 var yOffset = camY - (camY / groupZ);
                 zGroupTransform.position = new Vector3(xOffset, yOffset, groupZ);
             }
-            */
+            /*
             foreach (Transform zGroupTransform in transform)
             {
                 var origin = zGroupTransform.gameObject.GetComponent<Origin>().position;
@@ -36,6 +35,7 @@ namespace Filibusters
                 var yOffset = (camY - (camY / z)) + origin.y;
                 zGroupTransform.position = new Vector3(xOffset, yOffset, zGroupTransform.position.z);
             }
+            */
         }
     }
 }
