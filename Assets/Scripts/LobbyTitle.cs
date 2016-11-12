bba8afbdbde3c0f7f30d86c79370b5d99f7edfa6
Scott Munro <scottnmunro@gmail.com>
2016-11-12 11:48:37 -0600

    displaying "lobby name" (room name) at top of ready screen

---
+++ Assets/Scripts/LobbyTitle.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace Filibusters
+{
+    public class LobbyTitle : MonoBehaviour
+    {
+        void Start()
+        {
+            GetComponent<Text>().text += PhotonNetwork.room.name;
+        }
+    }
+}
