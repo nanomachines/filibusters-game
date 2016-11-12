c7aac1515afae3e40777556a36ff9a02439a8549
Scott Munro <scottnmunro@gmail.com>
2016-11-12 11:34:10 -0600

    two methods of parallax in: need to decide on one
    A: layer-based
    B: height-based
    
    cleanup tasks:
        * Skyscraper-X-Buffer/Group under GameMap>Outside
        * if i dont use B, clean up origin.cs and all of the skyscraper objs
            under parallax field
        * if I don't use A, cleanup the z-# game objects under parallax field
        * remove the unused method from parallax controller

---
+++ Assets/Scripts/ParallaxController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Filibusters
+{
+    public class ParallaxController : MonoBehaviour
+    {
+        [SerializeField]
+        float zFactor;
+        Transform mCameraTransform;
+
+        void Start()
+        {
+            mCameraTransform = GameObject.FindGameObjectWithTag(Tags.MAIN_CAMERA).transform;
+        }
+
+        void Update()
+        {
+            Vector2 cameraPos = mCameraTransform.position;
+            var camX = mCameraTransform.position.x;
+            var camY = mCameraTransform.position.y;
+            /*
+            foreach (Transform zGroupTransform in transform)
+            {
+                var groupZ = zGroupTransform.position.z;
+                var xOffset = camX - (camX / groupZ);
+                var yOffset = camY - (camY / groupZ);
+                zGroupTransform.position = new Vector3(xOffset, yOffset, groupZ);
+            }
+            */
+            foreach (Transform zGroupTransform in transform)
+            {
+                var origin = zGroupTransform.gameObject.GetComponent<Origin>().position;
+                var z = 1 / (zGroupTransform.localScale.y * .5f) * zFactor;
+                var xOffset = (camX - (camX / z)) + origin.x;
+                var yOffset = (camY - (camY / z)) + origin.y;
+                zGroupTransform.position = new Vector3(xOffset, yOffset, zGroupTransform.position.z);
+            }
+        }
+    }
+}
