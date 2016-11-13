18e2582e0cc0278819b01b2708815772273aeb77
Scott Munro <scottnmunro@gmail.com>
2016-11-12 23:54:01 -0600

    updated the buttons so that clicking on the button swaps out the sprite

---
+++ Assets/Scripts/SpriteSwapOnPress.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+namespace Filibusters
+{
+    public class SpriteSwapOnPress : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, ISubmitHandler
+    {
+        Image mButtonImage;
+        [SerializeField]
+        Sprite mPressedSprite;
+        Sprite mOriginalSprite;
+
+        void Start()
+        {
+            mButtonImage = GetComponent<Image>();
+            mOriginalSprite = mButtonImage.sprite;
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            mButtonImage.sprite = mPressedSprite;
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            mButtonImage.sprite = mOriginalSprite;
+        }
+
+        public void OnSubmit(BaseEventData eventData)
+        {
+            mButtonImage.sprite = mPressedSprite;
+        }
+    }
+}
