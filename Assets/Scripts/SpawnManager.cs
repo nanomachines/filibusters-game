5b6b0caec5130c44270b463c7208250f84d7acc8
Scott Munro <scottnmunro@gmail.com>
2016-11-17 20:38:24 -0600

    spawn points now don't place you randomly but instead place you at one of the 3 spawns that are the farthest average distance away from all players

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,11 +1,17 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
 
 namespace Filibusters
 {
+    struct SpawnOrderTuple
+    {
+        public float distance;
+        public int order;
+    }
+
     public class SpawnManager : MonoBehaviour
     {
         private GameObject[] SpawnPoints;
+        private SpawnOrderTuple[] mAveragePlayerDistanceToSpawn;
         GameObject LocalPlayer;
         private GameObject PlayerUI;
 
@@ -18,8 +24,9 @@ namespace Filibusters
             {
                 Instance = this;
                 SpawnPoints = GameObject.FindGameObjectsWithTag("Respawn");
+                mAveragePlayerDistanceToSpawn = new SpawnOrderTuple[SpawnPoints.Length];
                 LocalPlayer = PhotonNetwork.Instantiate(Utility.PlayerNumberToPrefab(NetworkManager.LocalPlayerNumber),
-                    GetRandomSpawnPoint(), Quaternion.identity, 0);
+                    SpawnPoints[NetworkManager.LocalPlayerNumber].transform.position, Quaternion.identity, 0);
                 LocalPlayer.GetComponent<SimplePhysics>().enabled = true;
                 LocalPlayer.GetComponent<AimingController>().enabled = true;
                 LocalPlayer.GetComponent<SwapButtonToggle>().enabled = true;
@@ -45,9 +52,73 @@ namespace Filibusters
             }
         }
 
-        public Vector3 GetRandomSpawnPoint()
+        public Vector3 GetFurthestAverageSpawnPoint(GameObject playerToSpawn)
         {
-            return SpawnPoints[Random.Range(0, SpawnPoints.Length)].transform.position;
+            GameObject[] players = GameObject.FindGameObjectsWithTag(Tags.PLAYER);
+            if (players.Length < 2)
+            {
+                return SpawnPoints[Random.Range(0, SpawnPoints.Length)].transform.position;
+            }
+
+            for (int i = 0; i < SpawnPoints.Length; ++i)
+            {
+                float avgDistance = 0;
+                Vector3 spawnPos = SpawnPoints[i].transform.position;
+                foreach (GameObject player in players)
+                {
+                    if (player != playerToSpawn)
+                    {
+                        avgDistance += Vector3.SqrMagnitude(spawnPos - player.transform.position);
+                    }
+                }
+                
+                avgDistance /= playerToSpawn == null ? players.Length : (players.Length - 1);
+                mAveragePlayerDistanceToSpawn[i].distance = avgDistance;
+                mAveragePlayerDistanceToSpawn[i].order = i;
+            }
+
+            /*
+            int furthestSpawnIndex = 0;
+            float furthestSpawnAvgDistance = mAveragePlayerDistanceToSpawn[0];
+            for (int i = 1; i < mAveragePlayerDistanceToSpawn.Length; ++i)
+            {
+                float nextAverageDistance = mAveragePlayerDistanceToSpawn[i];
+                if (furthestSpawnAvgDistance < nextAverageDistance)
+                {
+                    furthestSpawnIndex = i;
+                    furthestSpawnAvgDistance = nextAverageDistance;
+                }
+
+                else if (Mathf.Abs(furthestSpawnAvgDistance - nextAverageDistance) < Mathf.Epsilon)
+                {
+                    if (Random.Range(0, 1f) < 0.5f)
+                    {
+                        furthestSpawnIndex = i;
+                        furthestSpawnAvgDistance = nextAverageDistance;
+                    }
+                }
+            }
+
+            return SpawnPoints[furthestSpawnIndex].transform.position;
+            */
+            // sort in reverse order based on distance
+            System.Array.Sort(mAveragePlayerDistanceToSpawn, (x, y) => y.distance.CompareTo(x.distance));
+
+            int roll = Random.Range(0, 10);
+            int spawnIndex;
+            if (roll < 5)
+            {
+                spawnIndex = mAveragePlayerDistanceToSpawn[0].order;
+            }
+            else if (roll < 8)
+            {
+                spawnIndex = mAveragePlayerDistanceToSpawn[1].order;
+            }
+            else
+            {
+                spawnIndex = mAveragePlayerDistanceToSpawn[2].order;
+            }
+            return SpawnPoints[spawnIndex].transform.position;
         }
     }
 }
